becdc5388b22c482cfc611fef3c9e12ba0337b7d
Jaymar Polo <jaymarpolo@gmail.com>
2020-12-17 00:49:46 +0800

    3rd

--- crm/settings.py
+++ crm/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')qvonqf#6-x(i*y#ki1)c+elz@9_1_%ncx3a_l73%=-2^3iw4#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'DEMO_TEST',
         'USER': 'postgres',
-        'PASSWORD': 'tarroza69',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jaymarpolo@gmail.com'
-EMAIL_HOST_PASSWORD = 'Tarroza283781924'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
